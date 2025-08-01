fbe8da23a21cec5e9a709f5ea090be2a1d312c46
Rostislav Ivanov <x1qeee25@gmail.com>
2025-08-01 18:12:59 +0300

    gRPC practice. Составление протофайла

---
+++ second_grpc/course_client.py
@@ -0,0 +1,11 @@
+import grpc
+import course_service_pb2_grpc
+import course_service_pb2
+
+
+channel = grpc.insecure_channel("localhost:50051")
+stub = course_service_pb2_grpc.CourseServiceStub(channel=channel)
+
+response = stub.GetCourse(course_service_pb2.GetCourseRequest(course_id="Test api gRPC"))
+
+print(response)
